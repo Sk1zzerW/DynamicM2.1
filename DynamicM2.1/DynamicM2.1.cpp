1bc46e6c84474ad5dd030f388e8f2366471605de
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 09:57:57 +0200

    Добавьте файлы проекта.

---
+++ DynamicM2.1/DynamicM2.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void addColumn(int**& array, int rows, int currentCols, int* newColumn, int position) {
+    int** newArray = new int* [rows];
+    for (int i = 0; i < rows; i++) {
+        newArray[i] = new int[currentCols + 1];
+        int colIndex = 0;
+        for (int j = 0; j <= currentCols; j++) {
+            if (j == position) {
+                newArray[i][j] = newColumn[i];
+            }
+            else {
+                newArray[i][j] = array[i][colIndex++];
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++) delete[] array[i];
+    delete[] array;
+    array = newArray;
+}
